b91dded61f05c2e47912446496c4d2a2b03c579b
khbyun <novemberde@nate.com>
2018-02-18 21:57:34 +0900

    modify serverless webpack config entry

---
+++ app/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const slsw = require('serverless-webpack');
+const _ = require('lodash');
+
+module.exports = {
+  entry: {
+    api: './src/handler.ts'
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx',
+      '.json',
+      '.ts',
+      '.tsx'
+    ]
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js',
+  },
+  target: 'node',
+  module: {
+    loaders: [
+      { test: /\.ts(x?)$/, loader: 'ts-loader' },
+    ],
+  },
+};
