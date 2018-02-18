4f04b267b3b0df3247a9b47d5799f0a2d31dece2
khbyun <novemberde@nate.com>
2018-02-19 00:08:20 +0900

    add todo router

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,5 @@
-import * as express from 'express'
+import * as express from 'express';
+import todo from './routes/todo';
 
 
 export default class App {
@@ -10,10 +11,17 @@ export default class App {
     this.app.get('/', (req: express.Request, res: express.Response, next: express.NextFunction) => {
       res.send('hello world');
     });
+    this.app.use('/todo', todo);
 
-    this.app.get('/test', (req: express.Request, res: express.Response, next: express.NextFunction) => {
-      // throw new Error('hi');
-      res.send('TEST');
+    this.app.use('*', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+      return res.status(404).json({
+        message: `${req.url} is not found.`
+      });
+    });
+    this.app.use((err, req: express.Request, res: express.Response, next: express.NextFunction) => {
+      return res.status(500).json({
+        message: `Internal server error.`
+      });
     });
   }
 }
---
+++ src/routes/todo.ts
@@ -0,0 +1,41 @@
+import * as express from 'express';
+import _ from 'lodash';
+
+const router = express.Router();
+let todos = [{
+  id: 1,
+  title: 'Todo title',
+  content: 'Todo content'
+}];
+
+router.get('/', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  return res.json({
+    todos
+  });
+});
+router.get('/:id', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  const result = _.find(todos, {id: req.params.id});
+
+  if(!result) return res.status(404).send('Not Found');
+  return res.json(result);
+});
+router.post('/', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  return res.status(201).json({
+    body: req.body
+  });
+});
+router.put('/:id', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  const id = req.params.id;
+  return res.status(204).json({
+    body: req.body
+  });
+});
+router.delete('/:id', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  const id = req.params.id;
+
+  return res.status(204).json({
+    body: req.body
+  });
+});
+
+export default router;
